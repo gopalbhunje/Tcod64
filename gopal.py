e8f2cfb6db0ee97cabfa12ef67c6da14369fa24f
Gopal Bhunje <bhunjegopal4063@gmail.com>
2026-03-04 15:26:41 +0530

    Update gopal.py

--- gopal.py
+++ gopal.py
@@ -1,1 +1,54 @@
-print("Hello")
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking first number
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        // Taking second number
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        // Taking operator
+        System.out.print("Enter operator (+, -, *, /): ");
+        char operator = sc.next().charAt(0);
+
+        double result = 0;
+
+        switch(operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if(num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero");
+                    return;
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+
+        sc.close();
+    }
+}
